3adce148f1aecb20fba7ca95afbeaaf75c2d2109
Dipen Ved <vedipen@gmail.com>
2016-08-17 21:11:38 +0530

    Day 1 - Part 1

---
+++ src/day1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstdio>
+
+int main () {
+        using namespace std;
+        freopen("../resources/input/input-day1.txt","r",stdin);
+        freopen("output/output-day1.txt","w",stdout);
+        string input,temp;
+        getline(cin,input);
+        long before = input.length();
+        remove_copy(input.begin(),input.end(),back_inserter(temp),'(');
+        long after = temp.length();
+        long floor = before-(2*after);
+        printf("%ld",floor);
+        fclose(stdout);
+        return 0;
+}
